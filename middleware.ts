656e12103f8f2414cd6c683d42124b440c51e1d7
dulal <dll.mjee@gmail.com>
2026-03-04 23:04:59 +0530

    fix: simplify middleware

--- middleware.ts
+++ middleware.ts
@@ -2,9 +2,7 @@ import { NextResponse } from "next/server"
 import type { NextRequest } from "next/server"
 
 export function middleware(request: NextRequest) {
-  if (request.nextUrl.pathname === "/") {
-    return NextResponse.redirect(new URL("/login", request.url))
-  }
+  return NextResponse.redirect(new URL("/login", request.url))
 }
 
 export const config = {
