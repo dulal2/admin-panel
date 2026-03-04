80f8a6b97bd2d36f9315c488705cb2754f2f323f
dulal <dll.mjee@gmail.com>
2026-03-04 22:11:19 +0530

    fix: redirect home to login

--- app/page.tsx
+++ app/page.tsx
@@ -1,8 +1,5 @@
+import { redirect } from "next/navigation"
+
 export default function HomePage() {
-  return (
-    <main style={{ padding: 20 }}>
-      <h1>Admin Panel</h1>
-      <p>Go to <a href="/login">Login</a></p>
-    </main>
-  )
+  redirect("/login")
 }
